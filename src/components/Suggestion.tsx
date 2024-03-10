eaadcb8d77263eb1b04168fd6874c40f303f8315
Akash Gupta <akashgupta989322@gmail.com>
2024-03-10 18:14:12 +0530

    update

--- src/components/Suggestion.tsx
+++ src/components/Suggestion.tsx
@@ -6,44 +6,54 @@ import { useSelector } from "react-redux";
 import { configure } from "@/config/misc";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { Spinner } from "flowbite-react";
 const Suggestion = () => {
   const router = useRouter();
   const [profiles, setProfiles] = useState([]);
+  const [loading, setLoading] = useState(false);
   const user = useSelector((state: any) => state.user.user);
   const config = configure(user.token);
   const fetchProfile = async () => {
+    setLoading(true);
     const { data } = await axios.get("/api/user/suggestion", config);
     setProfiles(data.suggestedUsers);
+    setLoading(false);
   };
   useEffect(() => {
     fetchProfile();
   }, []);
   return (
     <div className="hidden md:block w-[25%] p-5">
-      <div className="border p-3 rounded">
-        <h1 className="text-xl font-bold border-b pb-2">Popular Profile</h1>
-        <div className="flex flex-col gap-3 mt-3 w-full">
-          {profiles?.map((profile: any) => (
-            <div
-              key={profile._id}
-              className="flex gap-2 cursor-pointer"
-              onClick={() => router.push(`/profile/${profile.username}`)}
-            >
-              <Image
-                src={profile.profilePicture}
-                width={45}
-                height={40}
-                alt="avatar"
-                className="rounded-full"
-              />
-              <div className="flex flex-col">
-                <b>{profile.name}</b>
-                <i className="text-sm ">{profile.username}</i>
-              </div>
-            </div>
-          ))}
+      {loading ? (
+        <div className="flex justify-center items-center mt-28">
+          <Spinner aria-label="Extra large spinner example" size="xl" />
         </div>
-      </div>
+      ) : (
+        <div className="border p-3 rounded">
+          <h1 className="text-xl font-bold border-b pb-2">Popular Profile</h1>
+          <div className="flex flex-col gap-3 mt-3 w-full">
+            {profiles?.map((profile: any) => (
+              <div
+                key={profile._id}
+                className="flex gap-2 cursor-pointer"
+                onClick={() => router.push(`/profile/${profile.username}`)}
+              >
+                <Image
+                  src={profile.profilePicture}
+                  width={45}
+                  height={40}
+                  alt="avatar"
+                  className="rounded-full"
+                />
+                <div className="flex flex-col">
+                  <b>{profile.name}</b>
+                  <i className="text-sm ">{profile.username}</i>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
